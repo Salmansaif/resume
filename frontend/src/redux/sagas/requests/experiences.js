138c6f8af3be49ac45994d692d1283447c367ce4
Salman Saif <salmanthedev@gmail.com>
2021-09-08 17:56:24 +0500

    saga url update

--- frontend/src/redux/sagas/requests/experiences.js
+++ frontend/src/redux/sagas/requests/experiences.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export function requestGetExperiences(props) {
   return axios.request({
     method: "get",
-    url: `http://127.0.0.1:8000/api/experiences/`
+    url: `https://salmansaif.herokuapp.com/api/experiences/`
   });
 }
--- frontend/src/redux/sagas/requests/projects.js
+++ frontend/src/redux/sagas/requests/projects.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export function requestGetProjects(props) {
   return axios.request({
     method: "get",
-    url: `http://127.0.0.1:8000/api/projects/`
+    url: `https://salmansaif.herokuapp.com/projects/`
   });
 }
