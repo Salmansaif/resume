609bf627ae8af0b67de3bfc506cdf5d1afa6e2be
Salman Saif <salmanthedev@gmail.com>
2021-09-08 22:31:55 +0500

    staticfiles

--- home/views.py
+++ home/views.py
@@ -28,12 +28,12 @@ class ExperienceListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class Assets(View):
-    def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+# class Assets(View):
+#     def get(self, _request, filename):
+#         path = os.path.join(os.path.dirname(__file__), 'static', filename)
 
-        if os.path.isfile(path):
-            with open(path, 'rb') as file:
-                return HttpResponse(file.read(), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound()
+#         if os.path.isfile(path):
+#             with open(path, 'rb') as file:
+#                 return HttpResponse(file.read(), content_type='application/javascript')
+#         else:
+#             return HttpResponseNotFound()
