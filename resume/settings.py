f4279ffc10ec95270aa37ff64379cea1e3da34fa
Salman Saif <salmanthedev@gmail.com>
2021-09-11 18:12:31 +0500

    settings update

--- resume/settings.py
+++ resume/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
